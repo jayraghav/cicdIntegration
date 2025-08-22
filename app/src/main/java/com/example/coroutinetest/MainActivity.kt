e830d46c6ff8e2de053f8d65a9bb25e1299e7109
Jayraghav <jayraghav.1990@gmail.com>
2025-08-22 16:55:58 +0530

    "asddsfsfsdfsdfdsfas"

--- app/src/main/java/com/example/coroutinetest/MainActivity.kt
+++ app/src/main/java/com/example/coroutinetest/MainActivity.kt
@@ -18,6 +18,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
